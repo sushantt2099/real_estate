e785711d38493bccc5d4567899ae13c52d7af7df
Sushant <forksushant@gmail.com>
2014-03-31 16:01:29 +0530

    update FacilityConstraint class to add label constant and change the view to use it

--- lib/FacilityConstraint.rb
+++ lib/FacilityConstraint.rb
@@ -2,8 +2,18 @@ require 'BasicConstraint'
 
 class FacilityConstraint < BasicConstraint
 
-	cattr_reader :parking_types
+	cattr_reader :parking_types, :LABEL_PARKING, :LABEL_GYM, :LABEL_SWIMMING_POOL, :LABEL_LIFT, :LABEL_AC 
 	#parking types
 	@@parking_types = create_types [:no, :bike, :car, :both ]
+
+	@@LABEL_PARKING = :parking
+
+	@@LABEL_GYM		= :gym
+
+	@@LABEL_LIFT	= :lift
+
+	@@LABEL_AC		= :ac
+
+	@@LABEL_SWIMMING_POOL	= "swimming pool"
 
 end
